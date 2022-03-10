123a3e3b9daabd51caee7ab7a939e2493e1e21ab
Frank Hermann <alphargon@gmail.com>
2022-03-10 17:33:24 +0100

    cleanup of old fisher routine

--- main.py
+++ main.py
@@ -46,38 +46,6 @@ plt.legend()
 plt.savefig("code_sim_res.pdf")
 plt.show()
 
-#%%
-
-sigma = np.sum([np.outer(np.roll(cc, k), np.roll(cc, k))*linalg.toeplitz(acf)
-               for k, acf in enumerate(acfs)], axis=0)
-sigma_inv = linalg.inv(sigma)
-
-
-#%%
-
-def sigma_deriv(k_p, Delta_p):
-    i = np.repeat(np.arange(n), n).reshape(n, n)
-    j = np.tile(np.arange(n), n).reshape(n, n)
-    delta = np.abs(i-j) == Delta_p
-    return np.outer(np.roll(cc, k_p), np.roll(cc, k_p))*delta
-
-def fisher(k1, k2, Delta1, Delta2):
-    A = np.matmul(sigma_inv, sigma_deriv(k1, Delta1))
-    B = np.matmul(sigma_inv, sigma_deriv(k2, Delta2))
-    return np.trace(np.matmul(A, B))/2
-
-def full_fisher():
-    r = np.zeros((N*M, N*M))
-    for k1 in range(N):
-        for Delta1 in range(1, M+1):
-            for k2 in range(N):
-                for Delta2 in range(1, M+1):
-                    r[k1*M + Delta1 - 1, k2*M + Delta2 - 1] = fisher(k1, k2, Delta1, Delta2)
-    return r
-
-
-F = full_fisher()
-F_inv = linalg.inv(F)
 
 #%%
 
@@ -85,7 +53,7 @@ sigma2 = np.sum([np.outer(np.roll(np.tile(c, 3), k), np.roll(np.tile(c, 3), k))*
                  for k, acf in enumerate(acfs)], axis=0)
 sigma2_inv = linalg.inv(sigma2)
 
-def fisher2(k_i, k_j, Delta_i, Delta_j):
+def fisher(k_i, k_j, Delta_i, Delta_j):
     eps_i = np.roll(c, k_i)*np.roll(c, k_i - Delta_i)
     eps_j = np.roll(c, k_j)*np.roll(c, k_j - Delta_j)
     eps_LL = np.outer(eps_i, eps_j)
@@ -97,7 +65,7 @@ def fisher2(k_i, k_j, Delta_i, Delta_j):
                sigma2_inv[len(c)+Delta_i:2*len(c)+Delta_i, 2*len(c)+Delta_j:3*len(c)+Delta_j - M])
     return (a + 2*b)*n/len(c)/2
 
-def full_fisher2():
+def fisher_matrix():
     F = np.zeros((N*M, N*M))
     for k1 in range(N):
         for Delta1 in range(1, M+1):
@@ -106,28 +74,24 @@ def full_fisher2():
                     p1 = k1*M + Delta1 - 1
                     p2 = k2*M + Delta2 - 1
                     if p2 >= p1:
-                        F[p1, p2] = (fisher2(k1, k2, Delta1, Delta2) +
-                                     fisher2(k1, k2, Delta1, -Delta2) +
-                                     fisher2(k1, k2, -Delta1, Delta2) +
-                                     fisher2(k1, k2, -Delta1, -Delta2))
+                        F[p1, p2] = (fisher(k1, k2, Delta1, Delta2) +
+                                     fisher(k1, k2, Delta1, -Delta2) +
+                                     fisher(k1, k2, -Delta1, Delta2) +
+                                     fisher(k1, k2, -Delta1, -Delta2))
                     else:
                         F[p1, p2] = F[p2, p1]
     return F
 
-F2 = full_fisher2()
-F2_inv = linalg.inv(F2)
-
-rng = np.random.default_rng(456318)
+F = fisher_matrix()
+F_inv = linalg.inv(F)
 
 theo_bound = sum(acf[0] for acf in acfs)/np.sqrt(2*len(cc))
 theo_bound_est = sum(acf[0] for acf in acfs)/np.sqrt(len(cc))
 
 for h, (m, s) in enumerate(zip(mean, std)):
-    #plt.plot(x, np.real(s), marker="x", ls="-", label=f"est. h={h}")
-    theo_limit = [np.sqrt(F_inv[h*M + Delta - 1, h*M + Delta - 1]/samples) for Delta in range(1, M + 1)]
-    plt.plot(x, theo_limit, marker="o", ls="--", label=f"theo. h={h}")
-    theo_limit = [np.sqrt(F2_inv[h*M + Delta - 1, h*M + Delta - 1]/samples) for Delta in range(1, M + 1)]
-    plt.plot(x, theo_limit, marker="x", ls="-", label=f"theo2. h={h}")
+    plt.plot(x, np.real(s), marker="x", ls="-", label=f"est. h={h}")
+    fisher_limit = [np.sqrt(F_inv[h*M + Delta - 1, h*M + Delta - 1]/samples) for Delta in range(1, M + 1)]
+    plt.plot(x, fisher_limit, marker="o", ls="--", label=f"theo. h={h}")
 plt.xlabel("lag index")
 plt.ylabel("standard deviation of power in a.u.")
 plt.legend()
