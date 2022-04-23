3335ede9fe8ffe46c611c6df464523a565c25c09
Frank Hermann <alphargon@gmail.com>
2022-04-23 22:46:05 +0200

    add correlation of range gates

--- modulation_verification.py
+++ modulation_verification.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import signal_calc as sig
+import scipy.linalg as linalg
 
 #M = 5
 #N = 7
@@ -15,27 +16,36 @@ n_code_tx = 2000  # number of full code transmissions (number of pulses in units
 n = (len(g) + rx_per_pulse - 1)*len(c)*n_code_tx  # total number of samples rx + tx
 off = len(c)  # offset in number of pulses
 w = (n_code_tx - 2)*len(c)  # number of pulses to process
-sim_corr = False  # simulate correlation between neighbouring range gates
+gen_method = "correlated_symmetric"  # simulate correlation between neighbouring range gates
 
 print("generating")
 rng = np.random.default_rng(42)
 t = np.arange(n)/(len(g) + rx_per_pulse - 1)
 layer_cnt = N*(len(g) + rx_per_pulse - 1)
-K_corrected = 10*(len(g) + rx_per_pulse - 1)  # need greater K (ACF cutoff) as time steps are shorter
-if sim_corr:
-    if layer_cnt%2 == 0:
-        layer_cnt += 1
-    layers = np.zeros((layer_cnt, n), dtype=np.complex128)
-    for h in range(0, layer_cnt, 2):
-        acf = sig.gen_layer_ACF(t, h, layer_cnt)
-        layers[h] = sig.layer_generator(acf, n, K_corrected)(rng)
-    for h in range(1, layer_cnt, 2):
-        layers[h, :-1] = (layers[h+1, :-1] + layers[h-1, :-1] + layers[h+1, 1:] + layers[h-1, 1:])/4
-else:
+K_corrected = 10*(len(g) + rx_per_pulse - 1)  # define greater K (ACF cutoff) as time steps are shorter
+if gen_method == "uncorrelated":  # overlapping ranges gates are uncorrelated
     layers = np.zeros((layer_cnt, n), dtype=np.complex128)
     for h in range(layer_cnt):
         acf = sig.gen_layer_ACF(t, h, layer_cnt)
         layers[h] = sig.layer_generator(acf, n, K_corrected)(rng)
+elif gen_method == "correlated_simple":  # average over two range gates to create correlation
+    layers = np.zeros((layer_cnt + 1, n), dtype=np.complex128)
+    for h in range(layer_cnt + 1):
+        acf = sig.gen_layer_ACF(t, h, layer_cnt + 1)
+        layers[h] = sig.layer_generator(acf, n, K_corrected)(rng)
+    for h in range(layer_cnt):
+        layers[h] = (layers[h] + layers[h + 1])/np.sqrt(2)
+    layers = layers[:-1]
+elif gen_method == "correlated_symmetric":  # generates one signal for each half range gate
+    layers = np.zeros((layer_cnt*2, n*2), dtype=np.complex128)
+    t = np.arange(2*n)/(len(g) + rx_per_pulse - 1)/2
+    for h in range(layer_cnt*2):
+        acf = sig.gen_layer_ACF(t, h, layer_cnt*2)
+        layers[h] = sig.layer_generator(acf, 2*n, K_corrected*2)(rng)
+    for layers in range(layer_cnt):
+        layers[h, :n] = (layers[2*h, ::2] + layers[2*h + 1, 1::2])/np.sqrt(2)
+    layers = layers[:layer_cnt, :n]
+
 print("encoding")
 columns = layers.reshape((layer_cnt, len(c)*n_code_tx, rx_per_pulse + len(g) - 1))[:, :, :rx_per_pulse]
 columns = np.swapaxes(columns, 0, 1)
@@ -63,15 +73,32 @@ res = np.array(res)
 
 #%%
 
-valid_sub_i = rx_per_pulse - len(g) + 1
-x = np.arange(1, M + 1)
-t_plot = np.linspace(0, M)
+K = rx_per_pulse - len(g) + 1  # basically the number of valid sub height indices
+
+def plot_result(res, fname):
+    x = np.arange(1, M + 1)
+    t_plot = np.linspace(0, M)
+    for h in range(N):
+        for w in [0, K - 1]:
+            plt.plot(t_plot, sig.gen_layer_ACF(t_plot, w + h*(len(g) + rx_per_pulse - 1) + len(g) - 1, layer_cnt), color="black", lw=1)
+            plt.scatter(x, np.real(res[h*K + w]), label=f"h={h}, w={w}")
+    plt.legend()
+    plt.savefig(fname)
+    plt.show()
+
+plot_result(res/len(g)**2, "direct.pdf")
+
+#%%
+
+A = np.correlate(g, g, mode="full")**2
+conv_mat = np.zeros((K, K), dtype=np.int32)
+for i in range(K):
+    l = min(len(g) - 1, i)
+    r = min(len(g), K - i)
+    conv_mat[i, i - l:i+r] = A[len(g) - 1 - l:len(g) - 1 + r]
+A = linalg.inv(conv_mat)
+res_deconv = np.zeros(res.shape, res.dtype)
 for h in range(N):
-    for w in [0, valid_sub_i - 1]:
-        plt.plot(t_plot, sig.gen_layer_ACF(t_plot, w + h*(len(g) + rx_per_pulse - 1) + len(g) - 1, layer_cnt), color="black", lw=1)
-        plt.scatter(x, np.real(res[h*valid_sub_i + w])/len(g)**2, label=f"h={h}, w={w}")
-plt.legend()
-plt.show()
-
-
-
+    for m in range(M):
+        res_deconv[K*h:K*(h + 1), m] = np.matmul(A, res[K*h:K*(h + 1), m])
+plot_result(res_deconv, "deconvolved.pdf")
