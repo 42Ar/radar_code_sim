fccfc3c299c0431103cad347e34723682147a4a2
Frank Hermann <alphargon@gmail.com>
2022-04-23 17:42:59 +0200

    make some arguments optional, and default to global config

--- signal_calc.py
+++ signal_calc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from config import N, n, K, method
+import config
 import numpy as np
 from scipy.linalg import sqrtm, toeplitz
 from scipy.fftpack import dct, fft
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 mode = "spread"
 
-def gen_layer_ACF(t, h):
+def gen_layer_ACF(t, h, N=config.N):
     if mode == "spread":
         width = 3*h/N + 1
         return (h/N + 1)*np.exp(-t/width)
@@ -28,14 +28,14 @@ def gen_layer_ACF(t, h):
             ACF[0] = 10
             return ACF
         
-def layer_generator(ACF):
-    if method == "direct":
+def layer_generator(ACF, n=config.n, K=config.K):
+    if config.method == "direct":
         T = sqrtm(toeplitz(ACF))
         def layer_gen(rand):
             a = np.matmul(T, rand.normal(size=n))
             b = np.matmul(T, rand.normal(size=n))
             return (a + 1j*b)/np.sqrt(2)
-    elif method == "spectral":
+    elif config.method == "spectral":
         # === relationship between the dct and fft used here
         # rng = np.random.default_rng(42)
         # N = 5
@@ -50,10 +50,10 @@ def layer_generator(ACF):
             a = fft(full_g*rand.normal(size=2*(n - 1)))[:n]
             b = fft(full_g*rand.normal(size=2*(n - 1)))[:n]
             return (a + 1j*b)/np.sqrt(2)
-    elif method == "hybrid":
+    elif config.method == "hybrid":
         ACF = ACF[:K]
         S = dct(ACF, type=1)
-        T = dct(np.sqrt(S)/(2*(K - 1)), type=1)  # apply convolution theorem ans solve for T
+        T = dct(np.sqrt(S)/(2*(K - 1)), type=1)  # apply convolution theorem and solve for T
         full_T = np.append(np.flip(T[1:]), T)
         def layer_gen(rand):
             a = rand.normal(size=n + 2*(K - 1))
@@ -62,7 +62,7 @@ def layer_generator(ACF):
             return np.correlate(full_T, c)
     return layer_gen
 
-def calculate_ACFs(rng, layer_gen, samples, M):
+def calculate_ACFs(rng, layer_gen, samples, M, n):
     r = np.empty((samples, M))
     for i in range(samples):
         d = layer_gen(rng)
@@ -71,16 +71,16 @@ def calculate_ACFs(rng, layer_gen, samples, M):
     return r
 
 if __name__ == "__main__":
-    t = np.arange(n)
+    t = np.arange(config.n)
     h = 0
     acf = gen_layer_ACF(t, h)
     layer_gen = layer_generator(acf)
     M = 5
     samples = 10000
     rng = np.random.default_rng(45)
-    r = calculate_ACFs(rng, layer_gen, samples, M)
+    r = calculate_ACFs(rng, layer_gen, samples, M, config.n)
     t_plot = np.linspace(0, M, 100)
-    plt.plot(t_plot, gen_layer_ACF(t_plot, h), color="black", lw=1)
+    plt.plot(t_plot, gen_layer_ACF(t_plot, h, config.N), color="black", lw=1)
     plt.errorbar(np.arange(1, M + 1),
                  np.mean(r, axis=0),
                  np.std(r, axis=0)/np.sqrt(samples),
