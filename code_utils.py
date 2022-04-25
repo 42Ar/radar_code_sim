7624bcb671650f61f33e53a1a0c442a2fab7554d
Frank Hermann <alphargon@gmail.com>
2022-04-25 19:09:37 +0200

    fix bug

--- code_utils.py
+++ code_utils.py
@@ -26,8 +26,11 @@ def full_code_check(c, N, M):
                 for Delta in range(1, M+1):
                     check_code(c, k, h, Delta, N, M)
                     
-def check_good_modulation_code(g):
+def check_good_modulation_code(g, max_acf):
     for s in range(1, len(g) - 1):
         k = sum(g[k]*g[k+s] for k in range(len(g) - s))
-        if abs(k) != (len(g) - s)%2:
-            print("modulation code error at shift {s}")
+        if (len(g) - s)%2 == 0:
+            if k != 0:
+                print(f"modulation code error at shift {s}")
+        elif abs(k) > max_acf:
+            print(f"modulation code error at shift {s}")
